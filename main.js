f4971770801176175cdfcf79e1815e85f6211867
ritesh-singh2004 <riteshsingh200495@gmail..com>
2025-11-22 00:18:30 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//Menu
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+};
+
+// Close Menu On Scroll
+Window.Onscroll = () => {
+     menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+};
+
+//ScrollReveal
+const animate = ScrollReveal ({
+    origin : 'top',
+    distance : '60px',
+    duration : '2500',
+    delay : '400',
+});
+
+animate.reveal (".home-text", {origin : "left"});
+animate.reveal (".home-img", {origin : "bottom"});
+animate.reveal (".heading", {origin : "top"});
+animate.reveal (".newsletter h2", {origin : "top"});
+animate.reveal (".header, .feature-box, .feature-menu-box, .item-box, .m-item-box, .t-box, .newsletter form",
+    {
+        interval : 100,
+    }
+);
